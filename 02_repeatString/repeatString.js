3eaf4e49fb42fedd11fb1662a7adddb2770e1c1e
FRANCO RODRIGUEZ PACIELLO <francorodriguez429@gmail.com>
2024-05-14 10:43:31 -0300

    foundations-ArraysAndLoops

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,21 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (word === '')
+        return '';
+
+    if(times === 0)
+        return '';
+
+    if(times > 0){
+        var newString = ""
+        for (let index = 0; index < times; index++) {
+            newString = newString +  word;                      
+        }
+
+        return newString;
+    }
+    else{
+        return 'ERROR'
+    }
 
 };
 
